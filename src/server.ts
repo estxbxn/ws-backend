a68e212328d6069915950188dd34288137cb2be4
Estéban Piecourt <esteban.piecourt@gmail.com>
2023-11-03 10:50:34 +0100

    🔥 refactor(server.ts): remove redundant CORS middleware
    The CORS middleware was removed as it was redundant and not necessary for the application.

--- src/server.ts
+++ src/server.ts
@@ -14,24 +14,6 @@ router.use(express.urlencoded({extended: false}));
 // Takes care of JSON Data
 router.use(express.json());
 
-// Rules of API
-router.use((req, res, next) => {
-	// set the CORS policy
-	res.header("Access-Control-Allow-Origin", "*");
-
-	// set the CORS headers
-	res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-	// set the CORS method headers
-	if (req.method === "OPTIONS") {
-		res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST PUT");
-		return res.status(200).json({});
-	}
-
-	// Next
-	next();
-});
-
 // Routes
 router.use("/", routes);
 
